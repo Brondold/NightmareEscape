5744ba9e5874fc371db53ab9bed77f902393ecb5
Zectalia <95757705+Zectalia@users.noreply.github.com>
2023-06-07 20:33:00 +0200

    Test ?

--- Assets/Générations Salles/Chest.cs
+++ Assets/Générations Salles/Chest.cs
@@ -9,10 +9,14 @@ public class Chest : MonoBehaviour
     public Transform itemHolder2;
     public Transform itemHolder3;
 
-    public GameObject SalleA;
-    public GameObject SalleB;
-    public GameObject SalleC;
-    public GameObject SalleD;
+    public GameObject Salle1;
+    public GameObject Salle2;
+    public GameObject Salle3;
+    public GameObject Salle4;
+    public GameObject Salle5;
+    public GameObject Salle6;
+    public GameObject Salle7;
+
 
     private void Start()
     {
@@ -43,10 +47,13 @@ public class Chest : MonoBehaviour
         WeightedRandomList<Transform> selectionSalles = new WeightedRandomList<Transform>();
 
         // Ajoutez vos éléments à la liste avec leurs poids respectifs à l'aide de la méthode Add()
-        selectionSalles.Add(SalleA.transform, 1);
-        selectionSalles.Add(SalleB.transform, 1);
-        selectionSalles.Add(SalleC.transform, 1);
-        selectionSalles.Add(SalleD.transform, 1);
+        selectionSalles.Add(Salle1.transform, 1);
+        selectionSalles.Add(Salle2.transform, 1);
+        selectionSalles.Add(Salle3.transform, 1);
+        selectionSalles.Add(Salle4.transform, 1);
+        selectionSalles.Add(Salle5.transform, 1);
+        selectionSalles.Add(Salle6.transform, 1);
+        selectionSalles.Add(Salle7.transform, 1);
 
         List<Transform> items = new List<Transform>();
         List<Transform> itemHolders = new List<Transform> { itemHolder1, itemHolder2, itemHolder3 };
--- Assets/TP/Teleportation.cs
+++ Assets/TP/Teleportation.cs
@@ -4,6 +4,7 @@ public class Teleportation : MonoBehaviour
 {
     public Transform teleportTarget; // L'endroit TP1
     public Transform teleportTarget1;
+    public Transform teleportTarget2;
     public float ScoreTP = 0;
 
     private CharacterController characterController;
@@ -43,6 +44,17 @@ public class Teleportation : MonoBehaviour
                 isTeleporting = false;
                 characterController.enabled = false;
                 characterController.transform.position = teleportTarget1.position; // Téléporte le personnage à l'endroit TP2
+                characterController.enabled = true;
+            }
+        }
+
+        if (ScoreTP == 3)
+        {
+            if (isTeleporting)
+            {
+                isTeleporting = false;
+                characterController.enabled = false;
+                characterController.transform.position = teleportTarget2.position; // Téléporte le personnage à l'endroit TP2
                 characterController.enabled = true;
             }
         }
